60cfb3d0280c861e5463f67239008d02271f4660
DMaximoVarela <maxivarela95@gmail.com>
2024-07-01 20:40:27 -0300

    añade animacion y cambio de banner

--- frontend/src/components/ServicesCarrousel/ServicesCarrousel.tsx
+++ frontend/src/components/ServicesCarrousel/ServicesCarrousel.tsx
@@ -1,18 +1,55 @@
+import { useState, useEffect } from "react";
 import NoNeonButton from "../NoNeonButton/NoNeonButton";
 import { MdArrowBackIosNew, MdArrowForwardIos } from "react-icons/md";
 import style from "./ServicesCarrousel.module.css";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
-interface serviceBanner {
-  name: string;
-  description: string;
-  image: string;
-}
+const services = [
+  {
+    name: "Reparación de Autos",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vel turpis velit. Sed sit amet lectus a metus pretium rhoncus. Integer malesuada ex id quam hendrerit, in placerat leo aliquet.",
+    image:
+      "https://images.unsplash.com/photo-1507578196479-eb569f365f5b?q=80&w=1533&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    name: "Diágnostico de Motor",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vel turpis velit. Sed sit amet lectus a metus pretium rhoncus. Integer malesuada ex id quam hendrerit, in placerat leo aliquet.",
+    image: "https://i.ibb.co/rZ59C9Y/paul-cisneros-vzw6mt-O7m-Yw-unsplash.jpg",
+  },
+  {
+    name: "Alineación de Ruedas",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vel turpis velit. Sed sit amet lectus a metus pretium rhoncus. Integer malesuada ex id quam hendrerit, in placerat leo aliquet.",
+    image: "https://i.ibb.co/Wv2QVxb/yuvraj-singh-tm-Ayn-VA-ih-E-unsplash.jpg",
+  },
+];
 
-const ServicesCarrousel: React.FC<serviceBanner> = ({
-  name,
-  description,
-  image,
-}) => {
+const ServicesCarrousel = () => {
+  const [currentSlide, setCurrentSlide] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      nextSlide();
+    }, 5000);
+
+    return () => clearInterval(interval);
+  }, [currentSlide]);
+
+  const nextSlide = () => {
+    setCurrentSlide((prevSlide) => (prevSlide + 1) % services.length);
+  };
+
+  const prevSlide = () => {
+    setCurrentSlide(
+      (prevSlide) => (prevSlide - 1 + services.length) % services.length
+    );
+  };
+
+  const { name, description, image } = services[currentSlide];
+
   return (
     <div
       className={style.container}
@@ -37,10 +74,16 @@ const ServicesCarrousel: React.FC<serviceBanner> = ({
           <NoNeonButton fondo="none" text="Solicitar Servicio" />
         </li>
         <ul className="flex justify-center items-center absolute bottom-[5%] right-0 mr-4 space-x-4">
-          <li className="w-[46px] 2xl:w-[52px] 4xl:w-[60px] h-[46px] 2xl:h-[52px] 4xl:h-[60px] text-[24px] 2xl:text-[28px] 4xl:text-[32px] rounded-full border-2 flex justify-center items-center cursor-pointer hover:text-ivory-white-700 hover:border-ivory-white-700">
+          <li
+            className="w-[46px] 2xl:w-[52px] 4xl:w-[60px] h-[46px] 2xl:h-[52px] 4xl:h-[60px] text-[24px] 2xl:text-[28px] 4xl:text-[32px] rounded-full border-2 flex justify-center items-center cursor-pointer hover:text-ivory-white-700 hover:border-ivory-white-700"
+            onClick={prevSlide}
+          >
             <MdArrowBackIosNew />
           </li>
-          <li className="w-[46px] 2xl:w-[52px] 4xl:w-[60px] h-[46px] 2xl:h-[52px] 4xl:h-[60px] text-[24px] 2xl:text-[28px] 4xl:text-[32px] rounded-full border-2 flex justify-center items-center cursor-pointer hover:text-ivory-white-700 hover:border-ivory-white-700">
+          <li
+            className="w-[46px] 2xl:w-[52px] 4xl:w-[60px] h-[46px] 2xl:h-[52px] 4xl:h-[60px] text-[24px] 2xl:text-[28px] 4xl:text-[32px] rounded-full border-2 flex justify-center items-center cursor-pointer hover:text-ivory-white-700 hover:border-ivory-white-700"
+            onClick={nextSlide}
+          >
             <MdArrowForwardIos />
           </li>
         </ul>
